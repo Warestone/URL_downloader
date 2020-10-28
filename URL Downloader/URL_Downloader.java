4b0e541c0ab90a23af5b0f9ea0d936a59e8adacd
John Warestone <48253995+Warestone@users.noreply.github.com>
2020-10-28 18:55:02 +0300

    Add files via upload

--- URL Downloader/URL_Downloader.java
+++ URL Downloader/URL_Downloader.java
@@ -10,20 +10,19 @@ public class URL_Downloader {
 
     public URL_Downloader(String[] args)
     {
-        if (args.length >= 1) {
-            try {url = new URL(args[0]);}
-            catch (MalformedURLException e) {
-                System.out.println("\n\nIncorrect URL '"+url.toString()+"'");
-                System.exit(-1);
-            }
-        }
-        if (args.length >= 2)path = new File(args[1]);                  // URL + path
         if (args.length == 0)throw new InputMismatchException();        // NO ARGS
         if (args[0].equals(""))throw new InputMismatchException();      // Empty URL
+        try {url = new URL(args[0]);}
+        catch (MalformedURLException e) {
+            System.out.println("\n\nIncorrect URL '"+url.toString()+"'");
+            System.exit(-1);
+        }
+        if (args.length >= 2)path = new File(args[1]);                  // URL + path
         if (path != null) path = new CheckPathFile().CheckPath(path);
+        boolean execute = args.length == 3;                             // URL + path + run
         GetInfoURL giURL = new GetInfoURL(url,path);
         String filename = giURL.getNameURL();
-        boolean execute = args.length == 3;                              // URL + path + run
+
         try
         {
             if (filename.contains(".html")) new SaveHTMLfile().saveHTML(url,path);                // if its html
